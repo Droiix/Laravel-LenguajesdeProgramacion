95f5f8429915ee2858fd89c3d0531ebccd031245
Josias <josiasperez77@gmail.com>
2025-05-15 19:33:53 -0500

    Agrega estructura base de proyecto Laravel con vistas y datos iniciales de BD

---
+++ app/Http/Controllers/ProyectosController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ejmplo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProyectosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $proyects = DB::table('ejmplos')->get();
+        return view("projects/index", ['proyectos' => $proyects]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ejmplo $ejmplo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ejmplo $ejmplo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ejmplo $ejmplo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ejmplo $ejmplo)
+    {
+        //
+    }
+}
